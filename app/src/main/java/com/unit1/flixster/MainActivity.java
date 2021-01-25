711b935cafbd10ca750cc3107d228f61aa416856
Karan Jeeten Thakkar <karan.thakkar97@gmail.com>
2021-01-25 11:54:58 -0600

    Initial commit

---
+++ app/src/main/java/com/unit1/flixster/MainActivity.java
@@ -0,0 +1,73 @@
+package com.unit1.flixster;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.unit1.flixster.adapters.MovieAdapter;
+import com.unit1.flixster.models.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Headers;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String TAG = "MainActivity";
+
+    List<Movie> movies;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RecyclerView rvMovies = findViewById(R.id.rvMovies);
+        movies = new ArrayList<>();
+
+        // Create adapter
+        final MovieAdapter movieAdapter = new MovieAdapter(this, movies);
+
+        // Attach adapter to RV
+        rvMovies.setAdapter(movieAdapter);
+
+        // Set Layout Manager
+        rvMovies.setLayoutManager(new LinearLayoutManager(this));
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess");
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    Toast.makeText(MainActivity.this, "Movie data loaded", Toast.LENGTH_SHORT).show();
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    Log.i(TAG, "Results " + results.toString());
+                    movies.addAll(Movie.fromJsonArray(results));
+                    movieAdapter.notifyDataSetChanged();
+                    Log.i(TAG, "Movies: " + movies.size());
+                } catch (JSONException e) {
+                    Log.e(TAG, "JSON exception, e");
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int i, Headers headers, String s, Throwable throwable) {
+                Log.d(TAG, "onFailure");
+            }
+        });
+    }
+}
